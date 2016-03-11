f4592e97d614d29eedc3efa6cd2c53bb64b9c291
Polina Nenchev <polina.mihova@yahoo.com>
2016-03-11 15:42:05 -0800

    add prompt package

--- js/cli.js
+++ js/cli.js
@@ -1,8 +1,17 @@
-var pingPong = require ('./ping-pong.js').pingPong;
+
+
+// This code will display all numbers up to 100 without asking that user to put a number into a required field
+// var pingPong = require ('./ping-pong.js').pingPong;
 // first create the function of Pingpong. require.... what is in the () is the route and/or which js file to go
 
-console.log('Ping-ponging up to 100!');
+// console.log('Ping-ponging up to 100!');
+
+// Asking the user the write a number of their choice using prompt();
+
+var goal = prompt ('What number would you like me to ping-pong up to ?');
 var result = pingPong(100);
 result.forEach(function(element) {
   console.log(element);
 });
+
+// after the code is writing i need to display in the terminal by first installing and running npm
