a210062512c628ec64309fbf999c85db463e5030
Polina Nenchev <polina.mihova@yahoo.com>
2016-03-12 14:48:57 -0800

    Update and add gitIgnore file

--- js/cli.js
+++ js/cli.js
@@ -7,11 +7,21 @@
 // console.log('Ping-ponging up to 100!');
 
 // Asking the user the write a number of their choice using prompt();
+var pingPong = require ('./ping-pong.js').pingPong;
+var prompt = require ('prompt');
+prompt.start();
 
-var goal = prompt ('What number would you like me to ping-pong up to ?');
-var result = pingPong(100);
-result.forEach(function(element) {
-  console.log(element);
+prompt.get('goal', function (err, result) {
+  var result = pingPong(result.goal);
+  result.forEach(function(element) {
+    console.log(element);
+  });
 });
+
+// var goal = prompt ('What number would you like me to ping-pong up to ?');
+// var result = pingPong(100);
+// result.forEach(function(element) {
+//   console.log(element);
+// });
 
 // after the code is writing i need to display in the terminal by first installing and running npm
